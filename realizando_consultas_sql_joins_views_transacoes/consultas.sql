ec876ee361040ba8d9e7836c23d7bca4ec2c8df0
Voidsspace <brenowillian07@gmail.com>
2026-03-04 18:25:35 -0300

    Add SQL queries for various data retrieval tasks

--- realizando_consultas_sql_joins_views_transacoes/consultas.sql
+++ realizando_consultas_sql_joins_views_transacoes/consultas.sql
@@ -130,3 +130,30 @@ DELETE from clientes where id = 27
 -- Sempre validar quando configurar nas tabelas on delete casct
 
 update pedidos set status = 'concluido'
+
+-- Traga todos os dados da cliente Maria Silva.
+select * from clientes where nome = 'Maria Silva'
+-- Busque o ID do pedido e o ID do cliente dos pedidos onde o status esteja como entregue.
+select id, idcliente from pedidos where status = 'Entregue'
+-- Retorne todos os produtos onde o preço seja maior que 10 e menor que 15.
+select * from produtos where preco > 10 and preco < 15 
+-- Busque o nome e cargo dos colaboradores que foram contratados entre 2022-01-01 e 2022-06-30.
+select nome, cargo from colaboradores where datacontratacao BETWEEN '2022-01-01' and '2022-06-30'
+-- Recupere o nome do cliente que fez o primeiro pedido.
+SELECT nome from clientes c 
+RIGHT join pedidos p on c.id = p.idcliente 
+order by datahorapedido limit 1
+
+--Liste os produtos que nunca foram pedidos.
+select * from itenspedidos where idproduto is NULL
+select pr.nome, i.idpedido, idproduto from produtos pr
+LEFT join itenspedidos i on pr.id = i.idproduto where i.idproduto is NULL
+
+--Liste os nomes dos clientes que fizeram pedidos entre 2023-01-01 e 2023-12-31.
+
+--Recupere os nomes dos produtos que estão em menos de 15 pedidos.
+
+--Liste os produtos e o ID do pedido que foram realizados pelo cliente 
+--"Pedro Alves" ou pela cliente "Ana Rodrigues".
+
+--Recupere o nome e o ID do cliente que mais comprou(valor) no Serenatto.
